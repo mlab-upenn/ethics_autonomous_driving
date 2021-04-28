a1183c5ee15b25597663abf0376016518c230450
mguru2019 <32654507+mguru2019@users.noreply.github.com>
2021-04-28 15:48:05 -0400

    Add files via upload

--- filter_scenario.py
+++ filter_scenario.py
@@ -95,126 +95,126 @@ def sort_scenario(scenario_id, scenario_loader, is_success):
                 global interstate_success
                 interstate_success += 1
         
-        if tag == Tag.URBAN:
+        elif tag == Tag.URBAN:
             global urban 
             urban += 1
             if is_success:
                 global urban_success 
-                urban_succes +=1
+                urban_success +=1
         
-        if tag == Tag.HIGHWAY:
+        elif tag == Tag.HIGHWAY:
             global highway 
             highway += 1
             if is_success:
                 global highway_success
                 highway_success += 1
                 
-        if tag == Tag.COMFORT:
+        elif tag == Tag.COMFORT:
             global comfort
             comfort += 1
             if is_success:
                 global comfort_success 
                 comfort_success += 1
                 
-        if tag == Tag.CRITICAL:
+        elif tag == Tag.CRITICAL:
             global critical
             critical += 1
             if is_success:
                 global critical_success
                 critical_success += 1
                 
-        if tag == Tag.EVASIVE:
+        elif tag == Tag.EVASIVE:
             global evasive
             evasive += 1
             if is_success:
                 global evasive_success
                 evasive_success += 1
                 
-        if tag == Tag.CUT_IN:
+        elif tag == Tag.CUT_IN:
             global cut_in
             cut_in += 1
             if is_success:
                 global cut_in_success
                 cut_in_success += 1
                 
-        if tag == Tag.ILLEGAL_CUTIN:
+        elif tag == Tag.ILLEGAL_CUTIN:
             global illegal_cutin
             illegal_cutin += 1
             if is_success:
                 global illegal_cutin_success
                 illegal_cutin_success += 1
                 
-        if tag == Tag.INTERSECTION:
+        elif tag == Tag.INTERSECTION:
             global intersection
             intersection += 1
             if is_success:
                 global intersection_success
                 intersection_success += 1
                 
-        if tag == Tag.LANE_CHANGE: 
+        elif tag == Tag.LANE_CHANGE: 
             global lane_change
             lane_change += 1
             if is_success:
                 global lane_change_success
                 lane_change_success += 1
                 
-        if tag == Tag.LANE_FOLLOWING:
+        elif tag == Tag.LANE_FOLLOWING:
             global lane_following
             lane_following += 1
             if is_success:
                 global lane_following_success
                 lane_following_success += 1
                 
-        if tag == Tag.MERGING_LANES:
+        elif tag == Tag.MERGING_LANES:
             global merging_lanes
             merging_lanes += 1
             if is_success:
                 global merging_lanes_success
                 merging_lanes_success += 1
                 
-        if tag == Tag.MULTI_LANE:
+        elif tag == Tag.MULTI_LANE:
             global multi_lane
             multi_lane += 1
             if is_success:
                 global multi_lane_success
                 multi_lane_success += 1
                 
-        if tag == Tag.ONCOMING_TRAFFIC:
+        elif tag == Tag.ONCOMING_TRAFFIC:
             global oncoming_traffic
             oncoming_traffic += 1
             if is_success:
                 global oncoming_traffic_success
                 oncoming_traffic_success += 1
                 
-        if tag == Tag.NO_ONCOMING_TRAFFIC:
+        elif tag == Tag.NO_ONCOMING_TRAFFIC:
             global no_oncoming_traffic
             no_oncoming_traffic += 1
             if is_success:
                 global no_oncoming_traffic_success
                 no_oncoming_traffic_success += 1
                 
-        if tag == Tag.PARALLEL_LANES:
+        elif tag == Tag.PARALLEL_LANES:
             global parallel_lanes
             parallel_lanes += 1
             if is_success:
                 global parallel_lanes_success
                 parallel_lanes_success += 1
                 
-        if tag == Tag.RACE_TRACK:
+        elif tag == Tag.RACE_TRACK:
             global race_track
             race_track += 1
             if is_success:
                 global race_track_success
                 race_track_success += 1
                 
-        if tag == Tag.ROUNDABOUT:
+        elif tag == Tag.ROUNDABOUT:
             global roundabout
             roundabout += 1
             if is_success:
                 global roundabout_success
                 roundabout_success += 1
                 
-        if tag == Tag.RURAL:
+        elif tag == Tag.RURAL:
             global rural
             rural += 1
             if is_success:
@@ -228,56 +228,56 @@ def sort_scenario(scenario_id, scenario_loader, is_success):
                 global simulated_success
                 simulated_success += 1
                 
-        if tag == Tag.SINGLE_LANE:
+        elif tag == Tag.SINGLE_LANE:
             global single_lane
             single_lane += 1
             if is_success:
                 global single_lane_success
                 single_lane_success += 1
                 
-        if tag == Tag.SLIP_ROAD:
+        elif tag == Tag.SLIP_ROAD:
             global slip_road
             slip_road += 1
             if is_success:
                 global slip_road_success
                 slip_road_success += 1
                 
-        if tag == Tag.SPEED_LIMIT:
+        elif tag == Tag.SPEED_LIMIT:
             global speed_limit
             speed_limit += 1
             if is_success:
                 global speed_limit_success
                 speed_limit_success += 1
                 
-        if tag == Tag.TRAFFIC_JAM:
+        elif tag == Tag.TRAFFIC_JAM:
             global traffic_jam
             traffic_jam += 1
             if is_success:
                 global traffic_jam_success
                 traffic_jam_success += 1
                 
-        if tag == Tag.TURN_LEFT:
+        elif tag == Tag.TURN_LEFT:
             global turn_left
             turn_left += 1
             if is_success:
                 global turn_left_success
                 turn_left_success += 1
                 
-        if tag == Tag.TURN_RIGHT:
+        elif tag == Tag.TURN_RIGHT:
             global turn_right
             turn_right += 1
             if is_success:
                 global turn_right_success
                 turn_right_success += 1
                 
-        if tag == Tag.TWO_LANE:
+        elif tag == Tag.TWO_LANE:
             global two_lane
             two_lane += 1
             if is_success:
                 global two_lane_success
                 two_lane_success += 1
                 
-        if tag == Tag.EMERGENCY_BRAKING:
+        elif tag == Tag.EMERGENCY_BRAKING:
             global emergency_braking
             emergency_braking += 1
             if is_success:
@@ -308,7 +308,7 @@ def print_stats():
     print(f"Slip road solutions found: {slip_road_success}/{slip_road}")
     print(f"Speed limit solutions found: {speed_limit_success}/{speed_limit}")
     print(f"Traffic jam solutions found: {traffic_jam_success}/{traffic_jam}")
-    print(f"Turn left solutions found: {turn_left_success}/{traffic_jam}")
+    print(f"Turn left solutions found: {turn_left_success}/{turn_left}")
     print(f"Turn right solutions found: {turn_right_success}/{turn_right}")
     print(f"Two lane solutions found: {two_lane_success}/{two_lane}")
     print(f"Emergency braking solutions found: {emergency_braking_success}/{emergency_braking}")
